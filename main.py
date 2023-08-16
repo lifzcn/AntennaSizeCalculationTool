be1e7b101798349187158b11985eb3180133464c
LiFZ <lifzcn@qq.com>
2023-08-16 10:42:50 +0800

    Update some files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Date       ：2023/8/15
 # Author     ：LiFZ
 # Email      ：lifzcn@gmail.com
-# Version    ：python 3.10
+# Version    ：python 3.10.0
 # Description：
 """
 
@@ -24,41 +24,50 @@ class mainWindow(QWidget, Ui_Form):
         self.pushButton_1.clicked.connect(self.calculate)
 
     def calculate(self):
-        c = 3.0 * np.power(10, 8)
+        self.lineEdit_3.clear()
+        self.lineEdit_4.clear()
+        self.lineEdit_5.clear()
+        self.lineEdit_6.clear()
+        self.lineEdit_7.clear()
+        self.lineEdit_8.clear()
 
-        # H = 0.762 * np.power(10, -3)
+        c = 3.0 * np.power(10.0, 8)
 
+        # 介质的介电常数
         epsilon_r = float(self.lineEdit_1.text())
 
-        f = float(self.lineEdit_2.text())
+        # 天线的中心工作频率
+        f = float(self.lineEdit_2.text()) * np.power(10.0, 9)
 
-        H = float(self.lineEdit_9.text())
+        # 介质基片的厚度
+        H = float(self.lineEdit_9.text()) * np.power(10.0, -3)
 
-        W = (c / 2 * f) * np.power((epsilon_r + 1) / 2, -1 / 2)
+        # 高效率辐射贴片的宽度
+        W = c / 2 / f * np.power((epsilon_r + 1) / 2, -1 / 2)
 
+        # 有效介电常数
         epsilon_e = (epsilon_r + 1) / 2 + (epsilon_r - 1) / 2 * np.power(1 + 12 * H / W, -1 / 2)
 
+        # 等效辐射缝隙长度
         Delta_L = 0.412 * H * (epsilon_e + 0.3) * (W / H + 0.264) / (epsilon_e - 0.258) / (W / H + 0.8)
 
+        # 介质内的导波波长
         lambda_e = c / f / np.sqrt(epsilon_e)
 
+        # 实际辐射单元长度
         L = c / f * np.sqrt(epsilon_r) - 2 * Delta_L
 
         xi_re = (epsilon_r + 1) / 2 + (epsilon_r - 1) / 2 * np.power(1 + 12 * H / L, -1 / 2)
 
+        # 馈电点位置
         L1 = L / 2 * (1 - 1 / np.sqrt(xi_re))
 
-        self.lineEdit_3.insert(str(W))
-
-        self.lineEdit_4.setText(str(L))
-
-        self.lineEdit_5.setText(str(lambda_e))
-
-        self.lineEdit_6.setText(str(epsilon_e))
-
-        self.lineEdit_7.setText(str(Delta_L))
-
-        self.lineEdit_8.setText(str(L1))
+        self.lineEdit_3.insert(str(round(W * np.power(10.0, 3), 3)) + " mm")
+        self.lineEdit_4.setText(str(round(L * np.power(10.0, 3), 3)) + " mm")
+        self.lineEdit_5.setText(str(round(lambda_e, 3)) + " m")
+        self.lineEdit_6.setText(str(round(epsilon_e, 3)))
+        self.lineEdit_7.setText(str(round(Delta_L * np.power(10.0, 3), 3)) + " mm")
+        self.lineEdit_8.setText(str(round(L1 * np.power(10.0, 3), 3)) + " mm")
 
 
 if __name__ == "__main__":
